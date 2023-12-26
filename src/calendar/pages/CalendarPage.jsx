f5d6cdf7f78f324e28f4b97f9680e166d9b16109
Eduardo <corl.facturacion@gmail.com>
2023-12-26 13:28:31 -0600

    FEAT-Added React Modal

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import { Calendar } from "react-big-calendar";
 import { addHours } from "date-fns";
-import { NavBar, CalendarEvent } from "../components";
+import { NavBar, CalendarEvent, CalendarModal } from "../components";
 import { localizer, getMessages } from "../../helpers";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
@@ -19,6 +20,10 @@ const events = [
 ];
 
 export const CalendarPage = () => {
+  const [lastView, setLastView] = useState(
+    localStorage.getItem("lastView") || "week"
+  );
+
   const eventStyleGetter = (event, start, end, isSelected) => {
     console.log({ event, start, end, isSelected });
     const style = {
@@ -30,6 +35,23 @@ export const CalendarPage = () => {
     return style;
   };
 
+  const onDoubleClick = (event) => {
+    console.log(
+      "🚀 ~ file: CalendarPage.jsx:34 ~ onDoubleClick ~ event:",
+      event
+    );
+  };
+
+  const onSelect = (event) => {
+    console.log("🚀 ~ file: CalendarPage.jsx:40 ~ onSelect ~ event:", event);
+    return {};
+  };
+
+  const onViewChanged = (event) => {
+    localStorage.setItem("lastView", event);
+    setLastView(event);
+  };
+
   return (
     <>
       <NavBar />
@@ -37,6 +59,7 @@ export const CalendarPage = () => {
         culture="es"
         localizer={localizer}
         events={events}
+        defaultView={lastView}
         startAccessor="start"
         endAccessor="end"
         style={{ height: "calc(100vh - 80px)" }}
@@ -45,7 +68,12 @@ export const CalendarPage = () => {
         components={{
           event: CalendarEvent,
         }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
       />
+
+      <CalendarModal />
     </>
   );
 };
