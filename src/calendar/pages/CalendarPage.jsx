0461a3cab86134ec623243b73b3ab919783a9b84
Eduardo <corl.facturacion@gmail.com>
2023-12-27 22:59:22 -0600

    FEAT-New notes with FabAddNew

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Calendar } from "react-big-calendar";
-import { NavBar, CalendarEvent, CalendarModal } from "../components";
+import { NavBar, CalendarEvent, CalendarModal, FabAddNew } from "../components";
 import { localizer, getMessages } from "../../helpers";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useUiStore, useCalendarStore } from "../../hooks";
@@ -66,6 +66,7 @@ export const CalendarPage = () => {
       />
 
       <CalendarModal />
+      <FabAddNew />
     </>
   );
 };
