10af6226fd8b7cc2b33591e83763ba4c1d3cdcc4
Eduardo <corl.facturacion@gmail.com>
2024-01-04 00:15:27 -0600

    FUNC-Deleted useEffect in useAuthStore & added Swa in login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,7 @@
-import "./LoginPage.css";
+import { useEffect } from "react";
 import { useAuthStore, useForm } from "../../hooks";
+import Swal from "sweetalert2";
+import "./LoginPage.css";
 
 const loginFormFields = {
   loginEmail: "",
@@ -14,7 +16,7 @@ const registerFormFields = {
 };
 
 export const LoginPage = () => {
-  const { startLogin } = useAuthStore();
+  const { startLogin, errorMessage } = useAuthStore();
 
   const {
     loginEmail,
@@ -44,6 +46,12 @@ export const LoginPage = () => {
       registerPassword2,
     });
   };
+
+  useEffect(() => {
+    if (errorMessage !== undefined) {
+      Swal.fire("Authentication Error", errorMessage, "error");
+    }
+  }, [errorMessage]);
 
   return (
     <div className="container login-container">
--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,15 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
-import { calendarApi } from "../api";
 import { clearErrorMessage, onChecking, onLogin, onLogout } from "../store";
-import { useEffect, useState } from "react";
+import { calendarApi } from "../api";
 
 export const useAuthStore = () => {
   const { status, user, errorMessage } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-  const [isError, setIsError] = useState(false);
 
   const startLogin = async ({ email, password }) => {
-    setIsError(false);
     dispatch(onChecking());
     try {
       const { data } = await calendarApi.post("/auth", { email, password });
@@ -22,20 +19,11 @@ export const useAuthStore = () => {
       }
     } catch (error) {
       dispatch(onLogout(error.message));
-    }
-  };
-
-  useEffect(() => {
-    if (isError) {
       setTimeout(() => {
         dispatch(clearErrorMessage());
-      }, 6000);
-
-      return () => {
-        clearTimeout();
-      };
+      }, 500);
     }
-  }, [isError]);
+  };
 
   return {
     //Props
