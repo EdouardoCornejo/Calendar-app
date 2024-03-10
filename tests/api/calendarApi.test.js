eb7ccfba36216dae5b559b75f8bd8bcc28eb4b4f
Eduardo <corl.facturacion@gmail.com>
2024-03-10 17:25:08 -0600

    FEAT-Added calendarApi test

--- tests/api/calendarApi.test.js
+++ tests/api/calendarApi.test.js
@@ -3,6 +3,18 @@ import calendarApi from '../../src/api/calendarApi';
 describe('Calendar API', () => {
 
     test('should have a config by default', () => {
-        console.log('calendarApi', calendarApi);
+
+        expect(calendarApi.defaults.baseURL).toBe(process.env.VITE_API_URL);
+    });
+
+    test('should have a x-token in header for all fetch', async () => {
+        localStorage.setItem('token', 'ABC123-XYZ');
+
+        try {
+            const res = await calendarApi.get('/auth/');
+            expect(res.config.headers['x-token']).toBe('ABC123-XYZ');
+        } catch (error) {
+        }
     });
 });
+
