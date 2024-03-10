206c5e911527d52ab7bd31b76872e7d3aeba055c
Eduardo <corl.facturacion@gmail.com>
2024-03-10 17:07:47 -0600

    FIX-Changed jest config

--- jest.setup.js
+++ jest.setup.js
@@ -1,19 +1,19 @@
-const { require } = require('commonjs');
-import jest from 'jest';
-import process from 'process';
-
+import dotenv from 'dotenv';
+// En caso de necesitar la implementación del FetchAPI
 // yarn add -D whatwg-fetch
 // import 'whatwg-fetch'; 
 
+// En caso de encontrar paquetes que lo requieran 
 // yarn add -D setimmediate
 // import 'setimmediate';
 
-
+// En caso de tener variables de entorno y aún no soporta el import.meta.env
 // yarn add -D dotenv
-require('dotenv').config({
+dotenv.config({
     path: '.env.test'
 });
 
+// Realizar el mock completo de las variables de entorno
 jest.mock('./src/helpers/getEnvVariables', () => ({
     getEnvVariables: () => ({ ...process.env })
 }));
---
+++ tests/api/calendarApi.test.js
@@ -0,0 +1,8 @@
+import calendarApi from '../../src/api/calendarApi';
+
+describe('Calendar API', () => {
+
+    test('should have a config by default', () => {
+        console.log('calendarApi', calendarApi);
+    });
+});
